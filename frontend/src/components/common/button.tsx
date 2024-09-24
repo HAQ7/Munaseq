1f64951e325629f4d1ce06236ec86cbd7824c519
Hussam Qannam <hussam.a.qannam@gmail.com>
2024-09-24 20:11:47 +0300

    added button animation

--- frontend/src/components/common/button.tsx
+++ frontend/src/components/common/button.tsx
@@ -3,17 +3,16 @@ export default function Button(props: {
     className?: string;
     outline?: boolean;
 }) {
-    console.log(props);
     if (props.outline) {
         return (
             <button
                 className={
-                    "relative bg-gradient-to-l h-10 p-[1.5px] from-[#AE00FE] to-[#652BB7] rounded-full grid place-items-center" +
+                    "relative bg-gradient-to-l h-10 p-[1.5px] from-[#AE00FE] to-[#652BB7] rounded-full grid place-items-center group " +
                     (props.className ? props.className : "")
                 }
             >
-                <div className="bg-white rounded-full h-full w-full grid place-items-center mx-4">
-                    <p className="bg-gradient-to-l from-[#AE00FE] to-[#652BB7] bg-clip-text text-transparent">{props.children}</p>
+                <div className="bg-white group-hover:bg-transparent rounded-full h-full w-full grid place-items-center mx-4 transition-all">
+                    <p className="bg-gradient-to-l from-[#AE00FE] to-[#652BB7] bg-clip-text text-transparent group-hover:text-white transition-all">{props.children}</p>
                 </div>
             </button>
         );
@@ -21,7 +20,7 @@ export default function Button(props: {
     return (
         <button
             className={
-                "px-4 h-10 bg-gradient-to-l from-[#AE00FE] to-[#652BB7] rounded-full text-white font-semibold grid place-items-center " +
+                "px-4 h-10 bg-[length:120%] hover:bg-right transition-all bg-gradient-to-l from-[#AE00FE] to-[#652BB7] rounded-full text-white font-semibold grid place-items-center " +
                 (props.className ? props.className : "")
             }
         >
--- frontend/src/components/landing-page/header.tsx
+++ frontend/src/components/landing-page/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="p-3 flex justify-between items-center">
             <a href="./">
-                <Image className="w-52" src={logo} alt="Munaseq logo" />
+                <Image priority className="w-52" src={logo} alt="Munaseq logo" />
             </a>
             <div className="flex gap-2">
                 <Button outline>تسجيل دخول</Button>
