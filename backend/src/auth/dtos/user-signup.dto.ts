0e14c2eb45f2b4ea6b47208a24ffb7e496734722
Modealf <moala057@gmail.com>
2024-10-01 10:20:57 +0300

    Change: now createUser() can add optional attributes for the user

--- backend/src/auth/dtos/user-signup.dto.ts
+++ backend/src/auth/dtos/user-signup.dto.ts
@@ -1,5 +1,6 @@
 // src/auth/dtos/user-signup.dto.ts
-import { IsEmail, IsString } from 'class-validator';
+import { Gender } from '@prisma/client';
+import { IsArray, IsBase64, IsEmail, IsEnum, IsJSON, IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class userSignUpDto {
   @IsEmail()
@@ -16,4 +17,33 @@ export class userSignUpDto {
 
   @IsString()
   username: string;
+
+  @IsOptional()
+  @IsString()
+  organizationName?: string;
+
+  @IsOptional()
+  @IsBase64()
+  profilePicture?: string; // Assume the profile picture is a URL
+
+  @IsOptional()
+  @IsString()
+  visibleName?: string; // Can be name or organization name
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  categories?: string[]; // Array of interests
+
+  @IsOptional()
+  @IsString()
+  description?: string; // Biography
+
+  @IsOptional()
+  @IsJSON()
+  socialAccounts?: object; // JSON object for social media accounts
 }
