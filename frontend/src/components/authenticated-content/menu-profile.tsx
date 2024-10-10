4f5f4098c72bc6685f9757f4e02151dfa25ecdc0
mohdAlmahmud <mohd.almahmud@gmail.com>
2024-10-10 18:47:18 +0300

    nothing

--- frontend/src/components/authenticated-content/menu-profile.tsx
+++ frontend/src/components/authenticated-content/menu-profile.tsx
@@ -4,45 +4,40 @@ import Image from "next/image";
 import convertBase64ToImage from "@/util/convertBase64ToImage";
 
 import { cookies } from "next/headers";
-import Dropdown from "./dropdown";
+import Dropdown from "../common/dropdown";
 import { redirect } from "next/navigation";
 
 export default async function MenuProfile() {
-    const cookiesStore = cookies();
-    const token = cookiesStore.get("token");
-    let data: any;
-    if (token) {
-        data = await getProfileAction(token.value);
-        return (
-            <div className="flex gap-3 p-5 items-center">
-                <div className="w-20 aspect-square relative rounded-full overflow-hidden">
-                    {data.profilePicture ? (
-                        <Image
-                            src={convertBase64ToImage(data.profilePicture)}
-                            alt="preview"
-                            fill
-                            priority
-                        />
-                    ) : (
-                        <Image
-                            src={userCircle}
-                            alt="user-circle"
-                            fill
-                            priority
-                        />
-                    )}
-                </div>
-                <div className="mt-2">
-                    <div className="font-bold text-lg text-nowrap overflow-ellipsis overflow-hidden w-44">
-                        {data.firstName + " " + data.lastName}
-                    </div>
-                    <div className="text-custom-gray text-nowrap overflow-ellipsis overflow-hidden w-44">
-                        {data.username}
-                    </div>
-                </div>
-                <Dropdown />
-            </div>
-        );
-    }
-    redirect("/signin");
+  const cookiesStore = cookies();
+  const token = cookiesStore.get("token");
+  let data: any;
+  if (token) {
+    data = await getProfileAction(token.value);
+    return (
+      <div className="flex gap-3 p-5 items-center">
+        <div className="w-20 aspect-square relative rounded-full overflow-hidden">
+          {data.profilePicture ? (
+            <Image
+              src={convertBase64ToImage(data.profilePicture)}
+              alt="preview"
+              fill
+              priority
+            />
+          ) : (
+            <Image src={userCircle} alt="user-circle" fill priority />
+          )}
+        </div>
+        <div className="mt-2">
+          <div className="font-bold text-lg text-nowrap overflow-ellipsis overflow-hidden w-44">
+            {data.firstName + " " + data.lastName}
+          </div>
+          <div className="text-custom-gray text-nowrap overflow-ellipsis overflow-hidden w-44">
+            {data.username}
+          </div>
+        </div>
+        <Dropdown />
+      </div>
+    );
+  }
+  redirect("/signin");
 }
